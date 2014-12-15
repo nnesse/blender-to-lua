b4b49e485d6bea9e2c92743c2ddfb15890c7d7bd
Neils Nesse <nnesse@sonic.net>
2014-12-15 15:38:13 -0800

    Refactor to reduce level of nesting

--- blender_rt_export.py
+++ blender_rt_export.py
@@ -40,14 +40,51 @@ def unregister():
 	bpy.utils.unregister_module(__name__)
 	bpy.types.INFO_MT_file_export.remove(menu_func_export)
 
-def dearray(a):
+
+def encode_prop_array(a, size):
 	b = []
 	for i in range(len(a)):
-		if type(a[i]).__name__ == 'bpy_prop_array':
-			b.append(dearray(a[i]))
-		else:
-			b.append(a[i])
+		b.append(encode_basic_property(a[i], size/len(a)))
 	return b
+
+def encode_basic_property(data, size):
+	type_name = type(data).__name__
+	if type_name in ('bool','float','string','str', 'int', 'tuple'):
+		out = data
+	elif type_name == "Vector":
+		out = data.to_tuple()
+	elif type_name == "Color":
+		out = (data.r, data.g, data.b)
+	elif type_name == "Matrix":
+		if size == 16:
+			out = data[0].to_tuple() + data[1].to_tuple() + data[2].to_tuple() + data[3].to_tuple()
+		elif size == 9:
+			out = data[0].to_tuple() + data[1].to_tuple() + data[2].to_tuple()
+		elif size == 4:
+			out = data[0].to_tuple() + data[1].to_tuple()
+	elif type_name == "Quaternion":
+		out = (data.x, data.y, data.z, data.w)
+	elif type_name == "Euler":
+		out = (data.x, data.y, data.z, data.order)
+	elif type_name == "bpy_prop_array":
+		out = encode_prop_array(data, size)
+	else:
+		out = "unknown:" + type_name
+	#TODO: 'set' types
+	return out
+
+def encode_property(structs, prop, data):
+	out = ()
+	if prop.fixed_type is None:
+		out = encode_basic_property(data, prop.array_length)
+	else:
+		if prop.type == "collection":
+			out = {}
+			for key, value in data.items():
+				out[key] = encode_struct(structs, structs[prop.fixed_type.identifier], value)
+		else:
+			out = encode_struct(structs, structs[prop.fixed_type.identifier], data)
+	return out
 
 def encode_struct(structs, struct, data):
 	out = {}
@@ -58,36 +95,7 @@ def encode_struct(structs, struct, data):
 		data_next = getattr(data, prop.identifier, None)
 		if data_next is None:
 			continue
-		if prop.fixed_type is None:
-			type_name = type(data_next).__name__
-			if type_name in ('bool','float','string','str', 'int', 'tuple'):
-				out[prop.identifier] = data_next
-			elif type_name == "Vector":
-				out[prop.identifier] = data_next.to_tuple()
-			elif type_name == "Color":
-				out[prop.identifier] = (data_next.r, data_next.g, data_next.b)
-			elif type_name == "Matrix":
-				if prop.array_length == 16:
-					out[prop.identifier] = data_next[0].to_tuple() + data_next[1].to_tuple() + data_next[2].to_tuple() + data_next[3].to_tuple()
-				elif prop.array_length == 9:
-					out[prop.identifier] = data_next[0].to_tuple() + data_next[1].to_tuple() + data_next[2].to_tuple()
-				elif prop.array_length == 4:
-					out[prop.identifier] = data_next[0].to_tuple() + data_next[1].to_tuple()
-			elif type_name == "Quaternion":
-				out[prop.identifier] = (data_next.x, data_next.y, data_next.z, data_next.w)
-			elif type_name == "bpy_prop_array":
-				out[prop.identifier] = dearray(data_next)
-			else:
-				out[prop.identifier] = "unknown:" + type_name
-			#TODO: handle 'Euler' and 'set' types
-		else:
-			if prop.type == "collection":
-				collection = {}
-				out[prop.identifier] = collection
-				for key, value in data_next.items():
-					collection[key] = encode_struct(structs, structs[prop.fixed_type.identifier], value)
-			else:
-				out[prop.identifier] = encode_struct(structs, structs[prop.fixed_type.identifier], data_next)
+		out[prop.identifier] = encode_property(structs, prop, data_next)
 	return out
 
 
